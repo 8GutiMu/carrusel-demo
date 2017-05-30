338874cb4a3576e74fe7c9345e6acd04ce3ee7fb
8GutiMu <you@example.com>
2017-05-30 14:56:36 -0500

    cambiando a jquery Listo

--- js/main.js
+++ js/main.js
@@ -1,26 +1,43 @@
-
-
+var target =0;
 $(document).ready( function(){
-    var target =0;
+    
     var $botones = $(".control")
     var $anterior = $(".previous")
     var $siguiente = $(".next")
     
     $botones.click(cambiarImagen)
-//    $anterior.click(anteriorImagen)
-//    $siguiente.click(siguienteImagen)
+    $anterior.click(anteriorImagen)
+    $siguiente.click(siguienteImagen)
 })
 
 
 function cambiarImagen(){
-    target = ($(this).data("target"))
-    
-//    mostrarImagen(target)
+//    target = (event.currentTarget)
+    target = parseInt($(this).data("target")) //otra forma de data
+    mostrarImagen(target)
     console.log(target)
 }
 
-//var mostrarImagen = function (target) {
-//	var lastSlide = $("div.active");
-//	var slide = $("div[data-slide='" + target + "']");
-//	console.log(lastSlide,slide)
-//};
+var mostrarImagen = function (target) {
+   
+	var $lastSlide = $("div.active");
+	var $slide = $("div[data-slide='" + target + "']");
+	$lastSlide.removeClass("active");
+	$slide.addClass("active");
+};
+
+var anteriorImagen = function (e) {
+	e.preventDefault();
+	target = target - 1;
+	target = (target < 0) ? 3 : target;
+	mostrarImagen(target);
+};
+
+var siguienteImagen = function (e) {
+	e.preventDefault();
+	target = target + 1;
+	target = (target > 3) ? 0 : target;
+	mostrarImagen(target);
+    mostrarImagen(target);
+};
+
